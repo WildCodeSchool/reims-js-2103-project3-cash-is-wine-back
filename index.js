3a38ddd26e62afbaf3da8682af0fa88f3c1d73d1
Timothee <augustin.timothee@gmail.com>
2021-06-02 12:13:00 +0200

    feat: config database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const connection = require('./db-config');
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+  } else {
+    console.log(`connected to database with threadId :  ${connection.threadId}`);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
