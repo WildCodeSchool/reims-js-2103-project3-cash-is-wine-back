0cf4d804928b22545efd4b0142da01f660fb543b
MarjorieThomassin <79426116+MarjorieThomassin@users.noreply.github.com>
2021-06-23 12:00:05 +0200

    created upload files fields route and add responses for request

--- routes/upload.js
+++ routes/upload.js
@@ -7,14 +7,29 @@ const upload = multer({ dest: 'tmp/' });
 const fs = require('fs');
 
 // define the index route
-uploadRoute.post('/', upload.single('File'), (req, res) => {
-  console.log(req.file);
-  if (req.file.size <= 100000) {
-    fs.renameSync(req.file.path, `uploads/${req.file.originalname}`);
+uploadRoute.post('/', upload.fields([{ name: 'bottleFront' }, { name: 'bottleBack' }]), (req, res) => {
+  const front = req.files.bottleFront[0];
+  console.log(front);
+
+  const back = req.files.bottleBack[0];
+  console.log(back);
+
+  let stringFront = 'L\'image recto a bien été importée';
+  let stringBack = 'L\'image verso a bien été importée.';
+
+  if (front.size <= 100000) {
+    fs.renameSync(front.path, `uploads/${front.originalname}`);
   } else {
-    fs.rmdirSync(req.file.path);
+    fs.rmSync(front.path);
+    stringFront = 'L\'image recto est trop volumineuse';
   }
-  res.send('Hello dear API client :)');
+  if (back.size <= 100000) {
+    fs.renameSync(back.path, `uploads/${back.originalname}`);
+  } else {
+    fs.rmSync(back.path);
+    stringBack = 'L\'image verso est trop volumineuse.';
+  }
+  res.send(`${stringFront}. ${stringBack}`);
 });
 
 module.exports = uploadRoute;
