d4d1c6d62c5d00e53875cd3fc8250c15e17f99c9
MarjorieThomassin <79426116+MarjorieThomassin@users.noreply.github.com>
2021-06-24 12:37:06 +0200

    fix: added json responses

--- routes/upload.js
+++ routes/upload.js
@@ -8,28 +8,43 @@ const fs = require('fs');
 
 // define the index route
 uploadRoute.post('/', upload.fields([{ name: 'bottleFront' }, { name: 'bottleBack' }]), (req, res) => {
-  const front = req.files.bottleFront[0];
-  console.log(front);
+  // console.log('hello');
+  // const front = req.files.bottleFront[0];
+  // const back = req.files.bottleBack[0];
+  // console.log(req.files.bottleFront);
+  // console.log(req.files.bottleBack);
+  // console.log(bottleFront[0]);
+  // if (front.size <= 100000) {
+  //   fs.renameSync(front.path, `uploads/${front.originalname}`);
+  // } else {
+  //   fs.rmSync(front.path);
+  // }
+  // if (back.size <= 100000) {
+  //   fs.renameSync(back.path, `uploads/${back.originalname}`);
+  // } else {
+  //   fs.rmSync(back.path);
+  // }
 
-  const back = req.files.bottleBack[0];
-  console.log(back);
-
-  let stringFront = 'L\'image recto a bien été importée';
-  let stringBack = 'L\'image verso a bien été importée.';
-
-  if (front.size <= 100000) {
-    fs.renameSync(front.path, `uploads/${front.originalname}`);
+  if (req.files.bottleFront[0].size <= 100000 && req.files.bottleBack[0].size <= 100000) {
+    fs.renameSync(req.files.bottleFront[0].path, `uploads/${req.files.bottleFront[0].originalname}`);
+    fs.renameSync(req.files.bottleBack[0].path, `uploads/${req.files.bottleBack[0].originalname}`);
+    res.status(200).json({
+      imageFront: req.files.bottleFront[0],
+      imageBack: req.files.bottleBack[0],
+    });
+  } else if (req.files.bottleFront[0].size > 100000 && req.files.bottleBack[0].size <= 100000) {
+    fs.rmSync(req.files.bottleFront[0].path);
+    fs.renameSync(req.files.bottleBack[0].path, `uploads/${req.files.bottleBack[0].originalname}`);
+    res.json({ error: 'Image recto trop volumineuse! Image verso bien uploadée!' });
+  } else if (req.files.bottleBack[0].size > 100000 && req.files.bottleFront[0].size <= 100000) {
+    fs.rmSync(req.files.bottleBack[0].path);
+    fs.renameSync(req.files.bottleFront[0].path, `uploads/${req.files.bottleFront[0].originalname}`);
+    res.json({ error: 'Image verso trop volumineuse! Image recto bien uploadée !' });
   } else {
-    fs.rmSync(front.path);
-    stringFront = 'L\'image recto est trop volumineuse';
+    fs.rmSync(req.files.bottleFront[0].path);
+    fs.rmSync(req.files.bottleBack[0].path);
+    res.json({ error: 'Les deux images sont trop volumineuses !!' });
   }
-  if (back.size <= 100000) {
-    fs.renameSync(back.path, `uploads/${back.originalname}`);
-  } else {
-    fs.rmSync(back.path);
-    stringBack = 'L\'image verso est trop volumineuse.';
-  }
-  res.send(`${stringFront}. ${stringBack}`);
 });
 
 module.exports = uploadRoute;
