57ed5480059c52f8237718c11829c5ca297afbbb
Timothee <augustin.timothee@gmail.com>
2021-07-08 12:07:06 +0200

    fix: updated sql file

--- cashiswine.sql
+++ cashiswine.sql
@@ -6,18 +6,32 @@ use cashiswine;
 create table `reference`(
   `id` int NOT NULL AUTO_INCREMENT,
   `appellation` varchar(100) NOT NULL,
-  `terroir` varchar(100) NULL,
+  `terroir` varchar(100),
   `label` varchar(10) NOT NULL,
   `color` varchar(100) NOT NULL,
-  `variety` varchar(100) NOT NULL,
+  `variety` varchar(100),
   `type` varchar(100) NOT NULL,
   `category` varchar(100) NOT NULL,
-  `reward` text NULL,
-  `precision` text NULL,
+  `reward` text,
+  `precision` text,
   `year` int NOT NULL,
-  `price` varchar(10) NOT NULL,  
+  `price` float NOT NULL,
+  `picture` text,  
   PRIMARY KEY (`id`)
 );
+
+insert into reference (appellation, terroir, label, color, variety, type, category, reward, `precision`, year, price, picture)
+  values 
+    ('Alsace Grand Cru', '', 'AOC-AOP', 'Blanc', 'Gewurztraminer', 'Moelleux', 'Région 1', '', 'Alsace grand cru concerne uniquement 50 lieuts-dit répartis sur le Haut Rhin et Bas Rhin', 2000, '22', 'Alsace.png'),
+    ('Brouilly', '', 'AOC-AOP', 'Rouge', 'Gamay N', 'Sec', 'Région 1', '', '', 2000, 16, 'Brouilly.png'),
+    ('Chablis Grand Cru', '', 'AOC-AOP', 'Blanc', 'Chardonnay', 'Sec', 'Région 1', '', '', 2000, 30, 'Chablis.png'),
+    ('Hautes Côtes de Beaune', '', 'AOC-AOP', 'Rouge', 'Pinot noir', 'Sec', 'Région 1', '', '', 2000, 18, 'Hautes-Côtes.png'),
+    ('Arbois', '', 'AOC-AOP', 'Blanc', 'Savagnin', 'Vin jaune', 'Région 1', '', '', 2000, 54, 'Vin-jaune.png'),
+    ('Chinon', '', 'AOC-AOP', 'Blanc', 'Chenin', 'Sec', 'Région 1', '', '2005 est une année exceptionnelle', 2000, 20, 'Chinon-Blanc.png'),
+    ('Chinon', '', 'AOC-AOP', 'Rouge', 'Cabernet-Franc N', 'Sec', 'Région 1', '', '', 2000, 24, 'Chinon-Rouge.png')
+  ;
+
+
 
 create table user(
   id INT PRIMARY KEY AUTO_INCREMENT,
