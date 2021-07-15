637933b3d530c09ce3f0332d180a186c2cb5c69e
Banzaipj <jolypascal20@gmail.com>
2021-07-15 15:35:06 +0200

    updated error message

--- routes/upload.js
+++ routes/upload.js
@@ -18,11 +18,11 @@ uploadRoute.post('/', upload.fields([{ name: 'bottleFront' }, { name: 'bottleBac
   } else if (req.files.bottleFront[0].size > 1000000 && req.files.bottleBack[0].size <= 1000000) {
     fs.rmSync(req.files.bottleFront[0].path);
     fs.renameSync(req.files.bottleBack[0].path, `uploads/${req.files.bottleBack[0].originalname}`);
-    res.json({ error: 'Image recto trop volumineuse! Image verso bien uploadée!', imageBack: req.files.bottleBack[0] });
+    res.json({ error: 'La bouteille a été ajoutée mais l\'étiquette est trop volumineuse!', imageBack: req.files.bottleBack[0] });
   } else if (req.files.bottleBack[0].size > 1000000 && req.files.bottleFront[0].size <= 1000000) {
     fs.rmSync(req.files.bottleBack[0].path);
     fs.renameSync(req.files.bottleFront[0].path, `uploads/${req.files.bottleFront[0].originalname}`);
-    res.json({ error: 'Image verso trop volumineuse! Image recto bien uploadée !', imageFront: req.files.bottleFront[0] });
+    res.json({ error: 'La bouteille a été ajoutée mais la contre-étiquette est trop volumineuse!', imageFront: req.files.bottleFront[0] });
   } else {
     fs.rmSync(req.files.bottleFront[0].path);
     fs.rmSync(req.files.bottleBack[0].path);
